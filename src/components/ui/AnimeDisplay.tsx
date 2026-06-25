1fb7f305c5e562ee2c0a697eb3d78a8e1f02b3ac
bluse01 <zukaguruli07@gmail.com>
2026-06-25 04:30:30 +0400

    working on AnimeDisplay component

--- src/components/ui/AnimeDisplay.tsx
+++ src/components/ui/AnimeDisplay.tsx
@@ -1,8 +1,34 @@
-import type AnimeObject from "@/types/Anime";
+import type { AnimeData, AnimePagination } from "@/types/Anime";
+import { Card } from "./card";
 
-export default function AnimeDisplay({ data, pagination }: AnimeObject) {
+interface AnimeDisplayProps {
+  data: AnimeData[];
+  pagination: AnimePagination;
+}
+
+const RenderCard = ({ data }: { data: AnimeData[] }) => {
+  return (
+    <>
+      {data.map((anime) => (
+        <Card key={anime.mal_id} className="w-xs">
+          <img
+            src={anime.images.webp.large_image_url}
+            alt="anime-img"
+            className="h-3/4"
+          />
+        </Card>
+      ))}
+    </>
+  );
+};
+
+export default function AnimeDisplay({ data, pagination }: AnimeDisplayProps) {
   console.log(data);
   console.log(pagination);
 
-  return <h1>test</h1>;
+  return (
+    <div className="grid grid-cols-3 gap-4 justify-center">
+      <RenderCard data={data} />
+    </div>
+  );
 }
--- src/types/Anime.ts
+++ src/types/Anime.ts
@@ -1,9 +1,26 @@
-interface AnimeData {
+export interface AnimeData {
   title: string;
+  title_english: string;
+  status: string;
+  score: number;
+  scored_by: number;
+  rank: number;
+  mal_id: number;
+  images: {
+    webp: {
+      image_url: string;
+      large_image_url: string;
+    };
+  };
 }
-export default interface AnimeObject {
-  data: AnimeData[];
+
+export interface AnimePagination {
   pagination: {
     current_page: number;
   };
+}
+
+export default interface AnimeObject {
+  data: AnimeData[];
+  pagination: AnimePagination;
 }
