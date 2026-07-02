1cc4924b9168d4bb744e7e198cae50125b27efa4
bluse01 <zukaguruli07@gmail.com>
2026-07-02 20:26:57 +0400

    reworked the pagination and removed useless useState

--- src/components/PageSwitcher.tsx
+++ src/components/PageSwitcher.tsx
@@ -7,60 +7,46 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 import { pageContext } from "@/context/pageContext";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 
 export function PaginationComp() {
   const { handlePageSwitch, pagination } = useContext(pageContext);
-  const [pages, setPages] = useState([1, 2, 3]);
   console.log(pagination);
 
-  const offsetPagLink = (currentTarget: number) => {
-    if (currentTarget === pages[2] && pagination?.has_next_page) {
-      setPages([pages[1], pages[2], pages[2] + 1]);
-      handlePageSwitch(pages[2]);
-    } else if (currentTarget === pages[0] && pages[0] - 1 !== 0) {
-      setPages([pages[0] - 1, pages[0], pages[1]]);
-      handlePageSwitch(pages[0]);
-    } else {
-      handlePageSwitch(pages[currentTarget]);
-    }
+  const currentPage = pagination?.current_page || 1;
+
+  const offsetPagLink = (targetPage: number) => {
+    console.log("test", currentPage + targetPage);
+    const calcMax = Math.max(1, currentPage + targetPage);
+    const calcMin = Math.min(pagination?.items.total || 3, calcMax);
+    handlePageSwitch(calcMin);
   };
 
-  const page = pagination?.current_page || 1;
-  console.log(page);
+  console.log(currentPage);
 
   return (
     <Pagination>
       <PaginationContent>
         <PaginationItem>
-          <PaginationPrevious onClick={() => offsetPagLink(page - 1)} />
+          <PaginationPrevious onClick={() => offsetPagLink(-1)} />
         </PaginationItem>
         <PaginationItem>
-          <PaginationLink
-            isActive={pagination?.current_page === pages[0]}
-            onClick={() => offsetPagLink(pages[0])}
-          >
-            {pages[0]}
+          <PaginationLink isActive onClick={() => offsetPagLink(-1)}>
+            {currentPage}
           </PaginationLink>
         </PaginationItem>
         <PaginationItem>
-          <PaginationLink
-            isActive={pagination?.current_page === pages[1]}
-            onClick={() => handlePageSwitch(pages[1])}
-          >
-            {pages[1]}
+          <PaginationLink onClick={() => offsetPagLink(1)}>
+            {currentPage + 1}
           </PaginationLink>
         </PaginationItem>
         <PaginationItem>
-          <PaginationLink
-            isActive={pagination?.current_page === pages[2]}
-            onClick={() => offsetPagLink(pages[2])}
-          >
-            {pages[2]}
+          <PaginationLink onClick={() => offsetPagLink(2)}>
+            {currentPage + 2}
           </PaginationLink>
         </PaginationItem>
         <PaginationItem>
-          <PaginationNext onClick={() => offsetPagLink(page + 1)} />
+          <PaginationNext onClick={() => offsetPagLink(1)} />
         </PaginationItem>
       </PaginationContent>
     </Pagination>
